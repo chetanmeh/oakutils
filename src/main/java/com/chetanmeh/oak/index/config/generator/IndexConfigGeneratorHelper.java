c9970b41b90dbb5a030a328de271c365f4744f17
Chetan Mehrotra <chetanm@adobe.com>
2016-04-04 23:32:11 +0530

    Change the default sample query

--- src/main/java/com/chetanmeh/oak/index/config/generator/IndexConfigGeneratorHelper.java
+++ src/main/java/com/chetanmeh/oak/index/config/generator/IndexConfigGeneratorHelper.java
@@ -3,18 +3,13 @@ package com.chetanmeh.oak.index.config.generator;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.io.StringReader;
-import java.util.Collections;
-import java.util.List;
 
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 
 public class IndexConfigGeneratorHelper {
-    public static NodeState getIndexConfig(String queryText) throws Exception {
-        return getIndexConfig(queryText, Collections.<String>emptyList());
-    }
 
-    public static NodeState getIndexConfig(String queryText, List<String> nodeTypes) throws Exception {
-        IndexConfigGenerator generator = new IndexConfigGenerator(nodeTypes);
+    public static NodeState getIndexConfig(String queryText) throws Exception {
+        IndexConfigGenerator generator = new IndexConfigGenerator();
         extractQueriesAndGenerateIndex(queryText, generator);
         NodeState result = generator.getIndexConfig();
         return result;
