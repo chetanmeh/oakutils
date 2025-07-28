9164b8f78ee7c8a46693d296d5ea7e265a16ad55
Thomas Mueller <thomasm@apache.org>
2025-07-28 19:44:02 +0200

    Redirect

--- src/main/webapp/WEB-INF/routes.groovy
+++
@@ -1,4 +0,0 @@
-all "/formatter", forward: "/WEB-INF/gsp/revisionFormatter.gsp"
-all "/analyze/index", forward: "/WEB-INF/gsp/analyzeIndex.gsp"
-all "/generate/index", forward: "/WEB-INF/gsp/generateIndex.gsp"
-all "/slack/query", forward: "/WEB-INF/groovy/slack.groovy"
